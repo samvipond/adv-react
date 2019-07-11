b2065e7ec31c6e351127153c5405f9e2d0a20f53
Sam Vipond <svipond@nexalogy.com>
2019-07-11 10:56:41 -0400

    start 40

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { Mutation } from 'react-apollo';
+import { TOGGLE_CART_MUTATION } from './Cart';
 import NavStyles from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
@@ -22,9 +24,12 @@ const Nav = () => (
             <Link href="/me">
               <a>Me</a>
             </Link>
-            <Link href="/signout">
-              <Signout />
-            </Link>
+            <Signout />
+            <Mutation mutation={TOGGLE_CART_MUTATION}>
+              {(toggleCart) => (
+                <button onClick={toggleCart}>My Cart</button>
+              )}
+            </Mutation>
           </>
         )}
         {!me && (
